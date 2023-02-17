59bd4f8e285491eb0856b06183a6c671755b3dbc
bekirbali <bfbali43@gmail.com>
2023-02-17 17:11:00 +0300

    odd in array

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// ! Date Formatter
+
 const today = new Date();
 
 function formatDate(date) {
@@ -7,3 +9,22 @@ function formatDate(date) {
   }).format(date);
 }
 console.log(formatDate(today));
+
+//! Find the odd repeat in array
+const text = [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5];
+
+function findOdd(arr) {
+  const oddObj = arr.reduce((sum, item) => {
+    if (!sum[item]) {
+      sum[item] = 0;
+    }
+    sum[item]++;
+    return sum;
+  }, {});
+  for (let [key, value] of Object.entries(oddObj)) {
+    if (value % 2) {
+      return key;
+    }
+  }
+}
+console.log(findOdd(text));
